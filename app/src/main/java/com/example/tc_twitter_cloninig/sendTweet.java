54f6b25f001db3a6b0b1f65ed870b552f5347911
Hamza <hamza.irfan1242@gmail.com>
2019-12-28 16:28:53 -0800

    viewing the other users posts

--- app/src/main/java/com/example/tc_twitter_cloninig/sendTweet.java
+++ app/src/main/java/com/example/tc_twitter_cloninig/sendTweet.java
@@ -3,24 +3,40 @@ package com.example.tc_twitter_cloninig;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
+import com.mikepenz.actionitembadge.library.ActionItemBadge;
+import com.mikepenz.actionitembadge.library.ActionItemBadgeAdder;
+import com.parse.FindCallback;
+import com.parse.LogOutCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class sendTweet extends AppCompatActivity {
     private EditText userTweet;
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_tweet);
         userTweet=findViewById(R.id.userTweet);
+        listView=findViewById(R.id.listView);
 
 
     }
@@ -60,4 +76,81 @@ public class sendTweet extends AppCompatActivity {
             });
         }
     }
+
+    public void userTweets(View v)
+    {
+        final ArrayList<HashMap<String ,String>>tweetList=new ArrayList<>();
+        final SimpleAdapter adapter=new SimpleAdapter(sendTweet.this,tweetList,
+                android.R.layout.simple_list_item_2,new String[]{"tweetUserName","tweetValue"},
+                new int []{android.R.id.text1,android.R.id.text2});
+        try {
+            ParseQuery<ParseObject> parseQuery=ParseQuery.getQuery("userTweets");
+            parseQuery.whereContainedIn("user",ParseUser.getCurrentUser().getList("followingOf"));
+            parseQuery.findInBackground(new FindCallback<ParseObject>() {
+                @Override
+                public void done(List<ParseObject> objects, ParseException e) {
+                    if(objects.size()>0 && e==null)
+                    {
+                        for(ParseObject tweetObjects: objects)
+                        {
+                            HashMap<String,String> userTweets=new HashMap<>();
+                            userTweets.put("tweetUserName",tweetObjects.getString("user"));
+                            userTweets.put("tweetValue",tweetObjects.getString("tweets"));
+                            tweetList.add(userTweets);
+
+
+                        }
+                        listView.setAdapter(adapter);
+                        listView.setVisibility(View.VISIBLE);
+                    }
+                }
+            });
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId()==R.id.logoutUser)
+        {
+            final ProgressDialog progressDialog=new ProgressDialog(sendTweet.this);
+            progressDialog.setMessage("Loading...");
+            progressDialog.show();
+            ParseUser.getCurrentUser().logOutInBackground(new LogOutCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if(e==null)
+                    {
+                        Intent intent=new Intent(sendTweet.this,homePage.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                    else
+                    {
+                        FancyToast.makeText(sendTweet.this,
+                                "Error", Toast.LENGTH_SHORT,
+                                FancyToast.ERROR,true).show();
+                    }
+                    progressDialog.dismiss();
+                }
+            });
+
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
